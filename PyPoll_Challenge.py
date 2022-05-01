d3ee82e888c2aa3e02a369680c24a3b33bc15524
cstern28 <102467249+cstern28@users.noreply.github.com>
2022-05-01 12:32:57 -0400

    final challenge

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -117,7 +117,7 @@ with open(file_to_save, "w") as txt_file:
     print(winning_county_summary)
 
     # 8: Save the county with the largest turnout to a text file.
-    txt_file.write(largest_county_turnout)
+    txt_file.write(winning_county_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
